b2c3c421b4af456024299f96a08d2b9a6a34220b
cfbj11 <cfbriones@uamv.edu.ni>
2024-09-27 08:46:25 -0600

    Subir

---
+++ Sesion12/Sesion12/models/clasecs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion12.models
+{
+    internal class clasecs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
